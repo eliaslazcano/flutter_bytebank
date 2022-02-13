21fba07ed36f13e476b3cfac7f733e6eb68e1c28
Elias Lazcano Castro Neto <elias304162@gmail.com>
2022-02-13 01:08:43 -0400

    Utilizando o ListView e StatefulWidget
    Utilizando o ListView na listagem de Transferências.

--- lib/views/transferencias.dart
+++ lib/views/transferencias.dart
@@ -3,24 +3,30 @@ import 'package:flutter_bytebank/components/item_transferencia.dart';
 import 'package:flutter_bytebank/model/transferencia.dart';
 import 'package:flutter_bytebank/views/formulario_transferencia.dart';
 
-class Transferencias extends StatelessWidget {
-  const Transferencias({Key? key}) : super(key: key);
+class Transferencias extends StatefulWidget {
+
+  Transferencias({Key? key}) : super(key: key);
+
+  @override
+  State<Transferencias> createState() => _TransferenciasState();
+}
+
+class _TransferenciasState extends State<Transferencias> {
+  final items = <Transferencia>[];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Transferências'),),
-      body: Column(
-        children: [
-          ItemTransferencia(Transferencia(4000,8738)),
-          ItemTransferencia(Transferencia(6000,3025)),
-        ],
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) => ItemTransferencia(items[index]),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () async {
           final Transferencia? transferencia = await Navigator.push(context, MaterialPageRoute(builder: (context) => FormularioTransferencia()));
-          debugPrint('$transferencia');
+          if (transferencia != null) setState(() => items.add(transferencia));
         },
       ),
     );
