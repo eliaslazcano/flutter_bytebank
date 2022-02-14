57d84207c044dc4306b85ec3844c439132822d37
Elias Lazcano Castro Neto <elias304162@gmail.com>
2022-02-14 16:21:35 -0400

    Refatorando
    simplificando código e removendo coisas desnecessárias.

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -4,12 +4,13 @@ import 'package:sqflite/sqflite.dart';
 
 class AppDatabase {
   static Future<Database> obterConexao() async {
-    final String diretorio = await getDatabasesPath();
-    final String fullpath = join(diretorio, 'bytebank.db');
-    final conexao = await openDatabase(fullpath, onCreate: (db, versao) {
-      db.execute('CREATE TABLE contatos (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, numero_conta INTEGER)');
-      db.execute('CREATE TABLE transferencias (id INTEGER PRIMARY KEY AUTOINCREMENT, valor REAL, numero_conta INTEGER)');
-    }, version: 1);
+    final String fullpath = join(await getDatabasesPath(), 'bytebank.db');
+    final conexao = await openDatabase(fullpath, version: 1,
+      onCreate: (db, version) {
+        db.execute('CREATE TABLE contatos (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, numero_conta INTEGER)');
+        db.execute('CREATE TABLE transferencias (id INTEGER PRIMARY KEY AUTOINCREMENT, valor REAL, numero_conta INTEGER)');
+      }
+    );
     return conexao;
   }
 
